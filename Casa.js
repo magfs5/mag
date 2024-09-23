8740d713c409d1c95a3e27fb0ad694a5724e7a88
magfs5 <magnafs03@gmail.com>
2024-09-23 14:58:25 -0300

    Update Casa.js

--- Casa.js
+++ Casa.js
@@ -1,1 +1,16 @@
+//usuário ira colocar peso e altura, o programa 
+//irá calcular o imc
+// se o imc for > 25 " a cima do peso ideal"
+//se o imc for >25 "peso ok"
 
+peso = 90
+altura = 1.60
+imc = 90 / 1.60
+
+se(imc>25 {
+  console.registro ('acima do peso')
+}
+else {console.registro('peso ok')
+
+    
+   
